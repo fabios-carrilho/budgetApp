15d65b65f8fefe987560b76ae2f161f248202967
Fabio Carrilho <fabios.carrilho@gmail.com>
2025-03-02 18:37:38 +0100

    Added comments

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+#Libraries and imports 
 from gui import BudgetApp
 import tkinter as tk
 
+#This is the main function that initialized the app
 if __name__ == "__main__":
     root = tk.Tk()
     app = BudgetApp(root)
